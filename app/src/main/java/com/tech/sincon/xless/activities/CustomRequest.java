b68fb79156bd377d681e26e9ab8ff96937b518a1
Peter Khamis <peterkhamis5@gmail.com>
2016-07-24 08:32:47 +0300

    Various changes

---
+++ app/src/main/java/com/tech/sincon/xless/activities/CustomRequest.java
@@ -0,0 +1,26 @@
+package com.tech.sincon.xless.activities;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by getcore03 on 7/24/2016.
+ */
+public class CustomRequest {
+
+
+}
